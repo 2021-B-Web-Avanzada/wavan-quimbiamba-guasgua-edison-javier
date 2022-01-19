02e18cc976053a6f43cc74e37a57fd3687d6c87b
Edison Quimbiamba <edison.quimbiamba@epn.edu.ec>
2022-01-18 22:35:17 -0500

    Angular

---
+++ Examen/examenQuimbiamba/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PerfilComponent,
+    FotosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
